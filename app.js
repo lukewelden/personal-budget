3f50a09b6b6aa6408e18c9babfbb772a7fe537a3
Luke Welden <lukewelden@hotmail.co.uk>
2023-11-04 08:13:14 +0000

    Removed unused users route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const envelopeRouter = require('./routes/envelope');
 
 const app = express();
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/envelope', envelopeRouter);
 
 // catch 404 and forward to error handler
